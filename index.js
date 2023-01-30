8c8edf3d646aadd4e5163afbad40c51ff8bee683
Abeer Ali Khan <aalkhan@students.uit.edu>
2023-01-31 03:10:17 +0500

    initialize grid, vertical, and horizontal arrays

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { World, Engine, Runner, Render, Bodies, Mouse, MouseConstraint } =
-  Matter;
+const { World, Engine, Runner, Render, Bodies } = Matter;
 
-const WIDTH = 800;
+const WIDTH = 600;
 const HEIGHT = 600;
+const WALL_SIZE = 30;
+const CELLS = 3;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -19,34 +20,30 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-  })
-);
-
 // Walls
 const walls = [
-  Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-  Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+  Bodies.rectangle(0, HEIGHT / 2, WALL_SIZE, HEIGHT, { isStatic: true }),
+  Bodies.rectangle(WIDTH, HEIGHT / 2, WALL_SIZE, HEIGHT, { isStatic: true }),
+  Bodies.rectangle(WIDTH / 2, 0, WIDTH, WALL_SIZE, { isStatic: true }),
+  Bodies.rectangle(WIDTH / 2, HEIGHT, WIDTH, WALL_SIZE, { isStatic: true }),
 ];
 
 walls.forEach((wall) => World.add(world, wall));
 
-// Random Shapes
-for (let i = 0; i <= 30; i++) {
-  if (Math.random() > 0.5) {
-    World.add(
-      world,
-      Bodies.rectangle(Math.random() * WIDTH, Math.random() * HEIGHT, 50, 50)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * WIDTH, Math.random() * HEIGHT, 35)
-    );
-  }
-}
+// Maze generation
+
+const grid = Array(CELLS)
+  .fill(null)
+  .map(() => Array(CELLS).fill(false));
+
+const verticals = Array(CELLS)
+  .fill(null)
+  .map(() => Array(CELLS - 1).fill(false));
+
+const horizontals = Array(CELLS - 1)
+  .fill(null)
+  .map(() => Array(CELLS).fill(false));
+
+console.log(grid);
+console.log(verticals);
+console.log(horizontals);
